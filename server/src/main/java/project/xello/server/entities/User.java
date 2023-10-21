ff57536de992259a1319b196a01ad6d97081c0e8
Metariz <pawmoeller94@gmail.com>
2023-10-22 01:07:02 +0200

    refactor to use annotation
    Instead of writing out all getters and setters

--- server/src/main/java/project/xello/server/entities/User.java
+++ server/src/main/java/project/xello/server/entities/User.java
@@ -6,7 +6,9 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import java.util.List;
+import lombok.Data;
 
+@Data
 @Entity
 public class User {
 
@@ -23,68 +25,5 @@ public class User {
 
   @OneToMany(mappedBy = "user")
   private List<Workspace> workspaces;
-
-  public Long getId() {
-    return id;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public String getEmail() {
-    return email;
-  }
-
-  public void setEmail(String email) {
-    this.email = email;
-  }
-
-  public String getUsername() {
-    return username;
-  }
-
-  public void setUsername(String username) {
-    this.username = username;
-  }
-
-  public String getFirstName() {
-    return firstName;
-  }
-
-  public void setFirstName(String firstName) {
-    this.firstName = firstName;
-  }
-
-  public String getLastName() {
-    return lastName;
-  }
-
-  public void setLastName(String lastName) {
-    this.lastName = lastName;
-  }
-
-  public String getLogin() {
-    return login;
-  }
-
-  public void setLogin(String login) {
-    this.login = login;
-  }
-
-  public String getPassword() {
-    return password;
-  }
-
-  public void setPassword(String password) {
-    this.password = password;
-  }
-
-  public List<Workspace> getWorkspaces() {
-    return workspaces;
-  }
-
-  public void setWorkspaces(List<Workspace> workspaces) {
-    this.workspaces = workspaces;
-  }
+  
 }

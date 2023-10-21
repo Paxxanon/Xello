648da21ef30602abbd90e0541e7270d56a836de3
Metariz <pawmoeller94@gmail.com>
2023-10-22 01:05:44 +0200

    add controller for /register and /users
    Basic controllers to get it working

--- server/src/main/java/project/xello/server/controllers/TestController.java
+++ server/src/main/java/project/xello/server/controllers/TestController.java
@@ -1,9 +1,16 @@
 package project.xello.server.controllers;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import project.xello.server.dtos.SignUpDto;
+import project.xello.server.dtos.UserDto;
 import project.xello.server.entities.User;
 import project.xello.server.services.UserService;
 
@@ -13,9 +20,20 @@ public class TestController {
 
   private final UserService userService;
 
-  //@PostMapping(path = "/register")
-  @GetMapping(path = "/users")
-  public @ResponseBody Iterable<User> getAllUsers() {
-    return userService.getUsers();
+  @PostMapping(path = "/register")
+  public ResponseEntity<UserDto> register(@RequestBody @Valid SignUpDto user) {
+    UserDto createdUser = userService.register(user);
+    return ResponseEntity.ok(createdUser);
   }
+
+  @GetMapping(path = "/users")
+  public ResponseEntity<Iterable<User>> getAllUsers() {
+    return new ResponseEntity<Iterable<User>>(
+      userService.getUsers(),
+      HttpStatus.OK
+    );
+  }
+  // public @ResponseBody Iterable<User> getAllUsers() {
+  //   return userService.getUsers();
+  // }
 }
